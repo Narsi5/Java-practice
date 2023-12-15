612e5f49604dcce2541d8ae427d7ebf99c802f63
Narsi5 <123388559+Narsi5@users.noreply.github.com>
2023-12-15 22:30:02 +0530

    Update and rename Main.java to ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class ReverseArray {
+        private static void reverse(int[] array){
+        for(int i=0;i<array.length;i++){
+            
+        }   
+        
+        System.out.println("Array" +" = "+ Arrays.toString(array));
+        int maxIndex = array.length -1;
+        int halfLength = array.length /2;
+        for(int i=0;i<halfLength;i++){
+            int temp = array[i];
+            array[i]=array[maxIndex-i];
+            array[maxIndex-i]=temp;
+        }
+        
+            System.out.println("Reversed array"+" = "+Arrays.toString(array));
+
+    }
+}
