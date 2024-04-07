542d2ee0023044c8af3e4361d24d850c95350c04
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-04-07 07:38:41 +0530

    adding programs

--- printpattern1.java
+++ printpattern1.java
@@ -1,5 +1,12 @@
 public class printpattern1 {
     public static void main(String[] args) {
+        /*
+        *****
+        *****
+        *****
+        *****
+        *****
+         */
         for(int i = 0;i<4;i++){
             for(int j = 0;j<5;j++){
                 System.out.print('*');
