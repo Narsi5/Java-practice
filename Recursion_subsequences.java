a706ca961155cdfc43fbec7cd199523cedddbedf
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-04-10 08:09:40 +0530

    Recursion to find no of subsequences in a string -6

---
+++ Recursion_subsequences.java
@@ -0,0 +1,19 @@
+public class Recursion_subsequences {
+    public static void subsequences(String str, int index, String newString){
+        if(index == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(index);
+
+        //to be
+        subsequences(str,index+1,newString+currChar);
+        //not to be
+        subsequences(str,index+1,newString);
+
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        subsequences(str,0,"");
+    }
+}
