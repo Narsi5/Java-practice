491b949885c9b4d8c556c22062f571a93c058d98
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-06-04 20:02:12 +0530

    graph using arraylist

---
+++ Graphlesson1.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Graphlesson1 {
+    static class Edge{
+        int source;
+        int destination;
+        int weight;
+
+        public Edge(int source, int destination,int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for (int i = 0; i < graph.length; i++) {
+            graph[i]  = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0,2 ,3));
+
+        graph[1].add(new Edge(1,2,4));
+        graph[1].add(new Edge(1,3,7));
+
+        graph[2].add(new Edge(2,0,8));
+        graph[2].add(new Edge(2,1,2));
+        graph[2].add(new Edge(2,3,1));
+
+        graph[3].add(new Edge(3,1,2));
+        graph[3].add(new Edge(3,2,5));
+    }
+    public static void main(String[] args) {
+        int V = 4;
+
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        for(int i = 0;i<graph[1].size();i++){
+            Edge e = graph[1].get(i);
+            System.out.print(e.destination+" ");
+        }
+    }
+}
